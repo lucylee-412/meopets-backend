3f1315442e2639fa2964052c99b30cdf4282f9b9
Marcin Tomala <marcin.p.tomala@gmail.com>
2022-01-25 19:13:19 -0500

    added ssl for heroku deploy

--- models/index.js
+++ models/index.js
@@ -4,7 +4,11 @@ const localpwd = ""
 
 /* to run our database, we define a sequelize object, passing it our database URL:
 (env variable for when the app is on Heroku, local db string for when the app is in local development) */
-const sequelize = new Sequelize(process.env.DATABASE_URL || `postgres://postgres:${localpwd}@localhost:5432/pets`);
+const sequelize = new Sequelize(process.env.DATABASE_URL || `postgres://postgres:${localpwd}@localhost:5432/pets`, {
+    dialectOptions: {
+        ssl:true
+    }
+});
 
 // db object will be the container for all the models we define in this directory:
 const db = {};
